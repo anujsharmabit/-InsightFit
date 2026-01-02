89fead71b1ba1ceed2d32e871eb3905fee0e9e2c
anujsharmabit <153370371+anujsharmabit@users.noreply.github.com>
2026-01-02 21:55:04 +0530

    Add files via upload

---
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    WEIGHT_LIFTING,
+    OTHER
+}
